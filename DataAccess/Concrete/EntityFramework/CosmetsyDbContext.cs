8b77886034784c0debaab29e8bc35f48d9cc40af
Aytacrustemli <aytacrustmli93@gmail.com>
2022-09-06 21:40:51 +0400

    06.09.2022

---
+++ DataAccess/Concrete/EntityFramework/CosmetsyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class CosmetsyDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=CosmetsyDB;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+
+        public DbSet<Category>
+    }
+}
